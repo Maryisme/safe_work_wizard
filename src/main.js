def01d810be619f87c17827e54eb1c40224ab8b2
Gurv-Chahal <gurveer.chahal15@icloud.com>
2025-10-25 15:25:25 -0700

    added vite so it runs on localhost

---
+++ src/main.js
@@ -0,0 +1,6 @@
+import './css/test.css';
+
+document.getElementById('app').innerHTML = `
+  <h1>Vite is running</h1>
+  <p>Edit <code>src/js/test.js</code> and <code>src/css/test.css</code>.</p>
+`;
