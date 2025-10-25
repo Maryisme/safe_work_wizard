90d3f807e5e38de2d1acd21440973e2477301890
Gurv-Chahal <gurveer.chahal15@icloud.com>
2025-10-25 16:19:58 -0700

    adding basic chatbox layout

--- src/main.js
+++ src/main.js
@@ -1,7 +1,3 @@
 
-import './css/test.css';
+<link rel="stylesheet" href="./index.css">
 
-document.getElementById('app').innerHTML = `
-  <h1>Vite is running</h1>
-  <p>Edit <code>src/js/test.js</code> and <code>src/css/test.css</code>.</p>
-`;
